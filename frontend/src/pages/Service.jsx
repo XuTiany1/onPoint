3a13e6ebf8da07771e6dad29d9e7897158633798
XuTiany1 <76981871+XuTiany1@users.noreply.github.com>
2023-08-04 09:58:44 -0400

    Successfully integrated form submission to backend, deleted the tofu api

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,6 +1,39 @@
-import Form from "../assets/Form";
+import { useState } from "react";
 
 function Service() {
+
+	const [url, setUrl] = useState('');
+	const [status, setStatus] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setStatus('loading');
+		setMessage('');
+		
+		try {
+			let res = await fetch("http://localhost:1000/testDB/postvideo", {
+			method: "POST",
+			body: JSON.stringify({
+			url: url,
+			}),
+		});
+		let resJson = await res.json();
+
+		if (res.status === 200) {
+			setUrl("")
+			setMessage("Url Successfully Uploaded");
+		} else {
+			setMessage("Some error occured");
+		}
+		} catch (err) {
+		console.log(err);
+		}
+		
+	};
+
+
+
     return (
 
         <div className = "min-h-screen flex flex-col ">
@@ -16,29 +49,22 @@ function Service() {
                 <div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-blue-400 bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
                     Hello
                 </div>
+	
 
-				<div> <Form/> </div>
 
-				{/* <form 
-					action="/databaseInit" 
-					method="post" 
-					id=""
-					name=""
-					target="">
+				<form onSubmit={handleSubmit} >
 
 					<div className="flex flex-col md:flex-row justify-center mb-4">
 						<input
 							placeholder="Your video link..."
 							type="url"
-							name="member[email]"
-							id="member_email"
+							value={url}
+							onChange={(e) => setUrl(e.target.value)}
 							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none rounded-full outline-none mb-4 md:mb-0"
 						/>
 						<input
 							type="submit"
 							value="SUBMIT!"
-							name=""
-							id=""
 							className=" bg-blue-300 md:rounded-tl-none md:rounded-bl-none rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
 						/>
 					</div>
@@ -47,7 +73,7 @@ function Service() {
 						After entering your video link, the summary will be generated down below!
 					</div>
 
-				</form> */}
+				</form>
 
 
 
