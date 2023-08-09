5eb8c2cda19ee47f3b9015b1526e40af7f302bed
XuTiany1 <76981871+XuTiany1@users.noreply.github.com>
2023-08-08 20:38:32 -0400

    major chnage to frontend interface

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,9 @@ function Home() {
         <div className = "min-h-screen flex flex-col ">
 
             <main className="container mx-auto px-6 pt-16 flex-1 text-center">
-                this is the HOME page, i am kinda tired today, and again today --- but imma do something tmr.., Ok... today also tired lol okokokokoko
+                So task for tomorrow =... FINISH AT LEAST THE VERY MINIMUM - Input audio, and receive a response generated automatically! 
+                I think using the 'use effect on the button is good!'
+                Search up some stuff!
             </main>
 
 
